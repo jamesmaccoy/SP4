712e4ca76b1e0ee6378cd0a74abcd884500cb6fb
jamesmaccoy <thankyou.digital@gmail.com>
2025-05-20 15:51:01 +0200

    Revert "Update page.client.tsx"
    This reverts commit 24ac5c6ca0ae114d3b2ed1d7423dd3593f16bfc3.

--- src/app/(frontend)/[slug]/page.client.tsx
+++ src/app/(frontend)/[slug]/page.client.tsx
@@ -155,9 +155,8 @@ const PageClient: React.FC<PageClientProps> = ({ page, draft, url }) => {
           </div>
 
           {/* Request Availability Button */}
-          <Button
-            variant="default"
-            className="px-4 py-2"
+          <button
+            className="px-4 py-2 bg-white text-gray-800 rounded-lg border border-gray-300 hover:bg-gray-100"
             onClick={() => {
               // Get rate from the page content
               let rate = '150' // Default rate
@@ -197,7 +196,7 @@ const PageClient: React.FC<PageClientProps> = ({ page, draft, url }) => {
             }}
           >
             Request Availability
-          </Button>
+          </button>
         </div>
       </article>
     )
